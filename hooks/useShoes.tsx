4d2ba5e11663c7f7268497a7a7bfa5ecee612721
anjankarmakar10 <anjankarmakar15@gmail.com>
2024-01-07 09:10:16 +0600

    implement optimization technique on useShoes

--- hooks/useShoes.tsx
+++ hooks/useShoes.tsx
@@ -1,23 +1,55 @@
 import { STARPI_URL } from "@/lib/constants";
 import { useEffect, useState } from "react";
 
+const getFetchData = async (category: string, controller: AbortController) => {
+  const URL =
+    category === "all shoes"
+      ? `${STARPI_URL}/api/shoes?populate=*`
+      : `${STARPI_URL}/api/shoes?filters[subCategories][name][$contains]=${category}&&populate=*`;
+
+  try {
+    const res = await fetch(URL, { signal: controller.signal });
+    const data: Response = await res.json();
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.message === "AbortError") return;
+    } else {
+      return "Someting went wrong";
+    }
+  }
+};
+
+// This function store cache values from previous request using emoization (memoization is an optimization technique)
+const optimizeDataFetching = (getData: Function) => {
+  const cache = {};
+  return async (category: string, controller: AbortController) => {
+    if (category in cache) {
+      console.log("From Cache");
+      return cache[category];
+    } else {
+      const value = await getData(category, controller);
+
+      cache[category] = value;
+      console.log("From API");
+      return value;
+    }
+  };
+};
+
+const getOptimizeData = optimizeDataFetching(getFetchData);
+
 const useShoes = (category: string) => {
   const [shoes, setShoes] = useState<Shoe[]>();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<String>();
 
   const getData = async (category: string, controller: AbortController) => {
-    const URL =
-      category === "all shoes"
-        ? `${STARPI_URL}/api/shoes?populate=*`
-        : `${STARPI_URL}/api/shoes?filters[subCategories][name][$contains]=${category}&&populate=*`;
-
     try {
       setLoading(true);
-      const res = await fetch(URL, { signal: controller.signal });
-      const data: Response = await res.json();
-      setLoading(false);
+      const data = await getOptimizeData(category, controller);
       setShoes(data.data);
+      setLoading(false);
     } catch (error) {
       if (error instanceof Error) {
         if (error.message === "AbortError") return;
@@ -30,7 +62,6 @@ const useShoes = (category: string) => {
   useEffect(() => {
     const controller = new AbortController();
     getData(category, controller);
-
     return () => controller.abort();
   }, [category]);
 
